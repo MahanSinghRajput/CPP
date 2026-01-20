0f17d6d7b84f17fcccc4217d8864cc75cb33098d
MahanSinghRajput <mahan.2428cse1312@kiet.edu>
2026-01-20 18:42:45 +0530

    added tricks

--- BitManipulation/tricks/checkSetBits.cpp
+++ BitManipulation/tricks/checkSetBits.cpp
@@ -2,7 +2,15 @@
 using namespace std;
 int main(){
     int n = 13, i = 2;
+    //using left shift operator
     if((n & (1 << i)) != 0){
+        cout << "Bit is set at position " << i << endl;
+    } else {
+        cout << "Bit is not set at position " << i << endl;
+    }
+
+    //using right shift operator
+    if((n >> i) & 1){
         cout << "Bit is set at position " << i << endl;
     } else {
         cout << "Bit is not set at position " << i << endl;
