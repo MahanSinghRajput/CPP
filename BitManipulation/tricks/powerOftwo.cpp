d6b937c05590d5f03a66a5531f3ebdf4610d2792
MahanSinghRajput <mahan.2428cse1312@kiet.edu>
2026-01-21 12:12:03 +0530

    added powerOfTwo

---
+++ BitManipulation/tricks/powerOftwo.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    if(n > 0 && (n & (n - 1)) == 0){
+        cout << n << " is a power of two." << endl;
+    } else {
+        cout << n << " is not a power of two." << endl;
+    }
+    return 0;
+}
